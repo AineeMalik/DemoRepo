e3249efe60d74e42b774eea8934d98bb1066734a
Ainee <qmalik@keyin.com>
2022-11-10 08:55:50 -0500

    First Commit

---
+++ AddMe.py
@@ -0,0 +1,4 @@
+first=float(input('Enter the first number:'))
+second=float(input('Enter the second number:'))
+result=first+second
+print('The sum is:'+str(result))
---
+++ Stuff.py
@@ -0,0 +1,7 @@
+def some_stuff(a, b):
+    return a * b
+
+
+x = 5
+y = 6
+print(some_stuff(x, y))
